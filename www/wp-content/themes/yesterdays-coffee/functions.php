c1d096a202a36a0ecf9985ec2cd4eda0e6e4ba74
gunji-kosuke <gun.kou-dev@outlook.com>
2020-05-11 18:36:41 +0900

    ファイル数を減らしてよりシンプルにしたいので不要そうなファイルを削除

--- www/wp-content/themes/yesterdays-coffee/functions.php
+++ www/wp-content/themes/yesterdays-coffee/functions.php
@@ -144,11 +144,6 @@ add_action( 'widgets_init', 'yesterdays_coffee_widgets_init' );
  */
 function yesterdays_coffee_scripts() {
 	wp_enqueue_style( 'yesterdays-coffee-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'yesterdays-coffee-style', 'rtl', 'replace' );
-
-	wp_enqueue_script( 'yesterdays-coffee-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-
-	wp_enqueue_script( 'yesterdays-coffee-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), _S_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
