472414f1e0a3cd27b28c23e8e8ea19a99ae0b53d
gunji-kosuke <gun.kou-dev@outlook.com>
2020-07-16 01:16:38 +0900

    プラグインAdvanced custom fieldを使用してコメントを拡張してコメントリスト内に表示

--- www/wp-content/themes/yesterdays-coffee/inc/custom-comment.php
+++ www/wp-content/themes/yesterdays-coffee/inc/custom-comment.php
@@ -28,6 +28,73 @@ function yesterdays_coffee_comment($comment, $args, $depth) {
             <div class="comment-content">
                 <?php comment_text(); ?>
             </div><!-- .comment-content -->
+
+           <ul>
+            <?php
+            if ( get_field('amount-beans', $comment) ):
+              $amount_beans = get_field_object('amount-beans', $comment);
+            ?>
+              <li>
+              <span><?php echo $amount_beans['label']; ?></span>
+              <span><?php echo $amount_beans['value']; ?></span>
+              </li>
+            <?php endif; ?>
+            <?php
+            if ( get_field('grind-size', $comment) ):
+              $grind_size = get_field_object('grind-size', $comment);
+            ?>
+              <li>
+              <span><?php echo $grind_size['label']; ?></span>
+              <span><?php echo $grind_size['choices'][ $grind_size['value'] ]; ?></span>
+              </li>
+            <?php endif; ?>
+            <?php
+            if ( get_field('temperature', $comment) ):
+              $temperature = get_field_object('temperature', $comment);
+            ?>
+              <li>
+              <span><?php echo $temperature['label']; ?></span>
+              <span><?php echo $temperature['value']; ?></span>
+              </li>
+            <?php endif; ?>
+            <?php
+            if ( get_field('extraction-time', $comment) ):
+              $extraction_time = get_field_object('extraction-time', $comment);
+            ?>
+              <li>
+              <span><?php echo $extraction_time['label']; ?></span>
+              <span><?php echo $extraction_time['value']; ?></span>
+              </li>
+            <?php endif; ?>
+            <?php
+            if ( get_field('extraction-amount', $comment) ):
+              $extraction_amount = get_field_object('extraction-amount', $comment);
+            ?>
+              <li>
+              <span><?php echo $extraction_amount['label']; ?></span>
+              <span><?php echo $extraction_amount['value']; ?></span>
+              </li>
+            <?php endif; ?>
+            <?php
+            if ( get_field('input-amount', $comment) ):
+              $input_amount = get_field_object('input-amount', $comment);
+            ?>
+              <li>
+              <span><?php echo $input_amount['label']; ?></span>
+              <span><?php echo $input_amount['value']; ?></span>
+              </li>
+            <?php endif; ?>
+            <?php
+            if ( get_field('steaming-time', $comment) ):
+              $steaming_time = get_field_object('steaming-time', $comment);
+            ?>
+              <li>
+              <span><?php echo $steaming_time['label']; ?></span>
+              <span><?php echo $steaming_time['value']; ?></span>
+              </li>
+            <?php endif; ?>
+            </ul>
+
         </article><!-- .comment-body -->
   <?php
 }
